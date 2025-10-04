9377971bdcd45aa8855d335f36b5e7c3d39b9753
Tobie1999 <tobiewerner@gmail.com>
2025-10-04 17:16:55 +0200

    Added post-processing extension to stitch.py

---
+++ stitch.py
@@ -0,0 +1,116 @@
+import argparse
+import re
+import skimage
+import numpy as np
+import nibabel as nib
+from pathlib import Path
+from functools import partial
+from utils import tqdm_
+from PIL import Image
+from skimage import morphology
+from skimage import filters
+
+def main(args: argparse.Namespace) -> None:
+    file_paths = sorted(args.data_folder.glob("*"))
+
+    m = re.compile(args.grp_regex)
+    patient_dict = {}
+    # get the paths to slices of each patient.
+    for fp in file_paths:
+        stem = fp.stem
+        match = re.match(m, stem)
+        id = match.group(1)
+
+        if id not in patient_dict:
+            patient_dict[id] = []
+        patient_dict[id].append(fp)
+
+    for id, paths in patient_dict.items():
+        # Load all the slices and stak them.
+        slices = []
+        for path in sorted(paths):
+            image = Image.open(path)
+            arr = np.array(image).astype(np.uint8)
+            slices.append(arr)
+
+        vol = np.stack(slices, axis=2) / 63
+        vol = vol.astype(np.uint8)
+
+        # Resize and save the predictions.
+        source_pattern = args.source_scan_pattern.replace("{id_}", id)
+        gt_img = nib.load(source_pattern)
+        gt = gt_img.get_fdata().astype(np.uint8)
+        vol = skimage.transform.resize(
+            vol,
+            gt.shape,
+            anti_aliasing=False,
+            order=0,
+            mode="constant",
+            preserve_range=True
+        ).astype(np.uint8)
+
+        vol = apply_postprocessing(vol, args.post_processing)
+
+        new_img = nib.Nifti1Image(vol, affine=gt_img.affine, header=gt_img.header)
+        args.dest_folder.mkdir(exist_ok=True, parents=True)
+        nib.save(new_img, args.dest_folder / (id + ".nii.gz"))
+
+
+def apply_postprocessing(volume, method="opening"):
+    match method:
+        case "opening":
+            return post_process_morphology(volume, skimage.morphology.opening)
+        case "closing":
+            return post_process_morphology(volume, skimage.morphology.closing)
+        case "gaussian_smoothing":
+            return gaussian_blur_thresholding(volume)
+        case _:
+            return volume
+
+
+def post_process_morphology(volume, post_function):
+    classes = np.unique(volume)
+    classes = classes[classes != 0]
+    footprint = morphology.ball(radius=2)
+    processed_volume = np.zeros_like(volume)
+
+    for class_label in classes:
+        mask = volume == class_label
+        processed_mask = post_function(mask, footprint=footprint)
+        processed_volume[processed_mask] = class_label
+
+    return processed_volume
+
+
+def gaussian_blur_thresholding(volume):
+    classes = np.unique(volume)
+    classes = classes[classes != 0]
+    processed_volume = np.zeros_like(volume)
+
+    for class_label in classes:
+        mask = volume == class_label
+        mask_smoothed = filters.gaussian(mask, sigma=1, preserve_range=True)
+        mask = mask_smoothed > 0.5
+        processed_volume[mask] = class_label
+
+    return processed_volume
+
+
+def get_args() -> argparse.Namespace:
+        parser = argparse.ArgumentParser(description="Stitching arguments")
+
+        parser.add_argument("--data_folder", type=Path, required=True)
+        parser.add_argument("--dest_folder", type=Path, required=True)
+        parser.add_argument("--num_classes", type=int, required=True)
+        parser.add_argument("--grp_regex", type=str, required=True)
+        parser.add_argument("--source_scan_pattern", type=str, required=True)
+        parser.add_argument("--post_processing", default="none", choices=["none", "opening", "closing", "gaussian_smoothing"])
+
+        args = parser.parse_args()
+
+        print(args)
+
+        return args
+
+if __name__ == "__main__":
+        main(get_args())
