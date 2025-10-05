a7b544e2c59aa1821f75fa03bf891c66bfdfb358
Tobie1999 <tobiewerner@gmail.com>
2025-10-05 13:19:20 +0200

    Added parser arguments for post processing settings

--- post_processing_utils.py
+++ post_processing_utils.py
@@ -3,22 +3,22 @@ from skimage import morphology
 from skimage import filters
 
 
-def apply_postprocessing(volume, method="opening"):
+def apply_postprocessing(volume, method="opening", radius=2, sigma=1):
     match method:
         case "opening":
-            return post_process_morphology(volume, morphology.opening)
+            return post_process_morphology(volume, morphology.opening, radius=radius)
         case "closing":
-            return post_process_morphology(volume, morphology.closing)
+            return post_process_morphology(volume, morphology.closing, radius=radius)
         case "gaussian_smoothing":
-            return gaussian_blur_thresholding(volume)
+            return gaussian_blur_thresholding(volume, sigma=sigma)
         case _:
             return volume
 
 
-def post_process_morphology(volume, post_function):
+def post_process_morphology(volume, post_function, radius=2):
     classes = np.unique(volume)
     classes = classes[classes != 0]
-    footprint = morphology.ball(radius=2)
+    footprint = morphology.ball(radius=radius)
     processed_volume = np.zeros_like(volume)
 
     for class_label in classes:
@@ -29,14 +29,14 @@ def post_process_morphology(volume, post_function):
     return processed_volume
 
 
-def gaussian_blur_thresholding(volume):
+def gaussian_blur_thresholding(volume, sigma=1):
     classes = np.unique(volume)
     classes = classes[classes != 0]
     processed_volume = np.zeros_like(volume)
 
     for class_label in classes:
         mask = volume == class_label
-        mask_smoothed = filters.gaussian(mask, sigma=1, preserve_range=True)
+        mask_smoothed = filters.gaussian(mask, sigma=sigma, preserve_range=True)
         mask = mask_smoothed > 0.5
         processed_volume[mask] = class_label
 
